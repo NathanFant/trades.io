7a68af5fb3189e400391d053907be90d5f20dcee
Trent Wilkins <trenttwilkins02@gmail.com>
2025-05-20 14:05:30 -0400

    non-ticket, boilerplate addition

---
+++ Backend/App/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/BCF"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
