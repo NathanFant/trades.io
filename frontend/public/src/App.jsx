4000516828cf848e1efc179cff448980771f42bc
Trent Wilkins <trenttwilkins02@gmail.com>
2025-05-21 15:11:17 +0000

    Update 4 files
    - /frontend/public/src/index.css
    - /frontend/public/src/App.jsx
    - /frontend/public/pages/HomePage.css
    - /frontend/public/pages/HomePage.jsx

--- frontend/public/src/App.jsx
+++ frontend/public/src/App.jsx
@@ -1,7 +1,9 @@
+import HomePage from "./pages/HomePage";
+
 export default function App() {
   return (
-    <>
-      <div>Hello!</div>
-    </>
-  )
+    <div>
+      <HomePage />
+    </div>
+  );
 }
