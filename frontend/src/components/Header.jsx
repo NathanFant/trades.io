330e2796623a9a925b566faa978165057b61615b
NathanF <nfant01@gmail.com>
2025-05-22 10:18:02 -0500

    s header comp built

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
     const navigate = useNavigate();
+    const { loggedIn } = useContext(UserContext);
 
 
 
@@ -10,18 +11,22 @@ export default function Header() {
         <>
             <header>
                 <h2 className="homepage-link">BCF.COM</h2>
+                <div className="right-header-container">
+                    {!loggedIn ? (
+                        <button className="login-button" onClick={() => alert("Login clicked")}>Login</button>
+                    ) : (
+                        <>
+                        <button className="postjob-button" onClick={() => alert("Post a job clicked")}>Post a Job</button>
+                        <button className="profile-button" onClick={() => alert("Profile clicked")}>Profile</button>
+                        </>
+                    )}
+               </div>
+
             </header>
         </>
     )
 }
 
-{/* <header className="homepage-header">
-        <h1>BCF.COM</h1>
-        <div><h2>Welcome to Blue Collar Fiverr</h2></div>
-        <div className="header-buttons">
-          <button onClick={() => alert("Login clicked")}>Login</button>
-          <button onClick={() => alert("Post Job clicked")}>Post a Job</button>
-        </div>
-      </header> */}
-
 {/* Home | Search for jobs... | Login  (if logged in, Post a Job | Profile) */}
+{/* We decided to take out search for jobs on all pages, except for the home page. */}
+{/* login-postjob-container will be aligned to the right side, pending header css */}
