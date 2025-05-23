4c051098e44b12d61281173dba6f5f0ecbb1297c
your name <danjump2@gmail.com>
2025-05-23 13:12:29 -0500

    Updated request button, Dan and Trent

--- frontend/src/components/RequestButton.jsx
+++ frontend/src/components/RequestButton.jsx
@@ -1,21 +1,31 @@
+import { useUser } from "../context/UserContext";
+
 
 export default function RequestButton({ job }) {
+    const { user } = useUser();
+
 
     async function handleRequestJob() {
+
         try {
             const res = await fetch("http://localhost:8000/requests", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
                     listing_id: job?.poster_id,
-                    worker_id: 1
+                    worker_id: user?.user_id
                 }),
             });
 
             if (!res.ok) {
-            throw new Error("Failed to send job request");
+                if (res.status === 400) {
+                    alert("Job already requested");
+                    return
+                }
+
+
             }
             alert("Job request sent to user:", job?.poster_id)
 
@@ -24,12 +34,11 @@ export default function RequestButton({ job }) {
         }
     }
 
-
-  return (
-    <button onClick={(e) => {
-        e.stopPropagation();
-        handleRequestJob();
-    }}
-    >Request Job</button>
-  )
+    return (
+        <button onClick={(e) => {
+            e.stopPropagation();
+            handleRequestJob();
+        }}
+        >Request Job</button>
+    )
 }
