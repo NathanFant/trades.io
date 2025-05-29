615a1f7a2fdda3b1b304de97ce507927fd158026
Vincent Gallo <vincentgallo@Vincents-MacBook-Pro.local>
2025-05-29 12:03:07 -0400

    added name and listing card to profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,45 @@
 import Skills from "../components/Skills";
-
+import { useUser } from "../context/UserContext";
+import ListingCard from "../components/ListingCard";
+import { useEffect, useState } from "react";
 
 export default function Profile() {
+    const { user } = useUser()
+    const [listings, setListings] = useState([]);
+    const [expandedId, setExpandedId] = useState(null);
+
+    useEffect(() => {
+        const fetchListings = async () => {
+          try {
+            const res = await fetch(`http://localhost:8000/users/${user?.user_id}/listings`);
+            if (!res.ok) throw new Error("Failed to fetch listings");
+            const data = await res.json();
+            setListings(data);
+          } catch (err) {
+            console.error("Error fetching listings:", err);
+          }
+        };
+
+        fetchListings();
+    }, [user?.user_id]);
+
+    return (
+        <>
+        <div>{user?.username}</div>
+        <Skills />
+        {listings.map((job, index) => (
+            <ListingCard
+                key={index}
+                job={job}
+                expandedId={expandedId}
+                setExpandedId={setExpandedId}
+            />
+        ))}
 
 
-    return <Skills />
+        </>
 
+
+
+    )
 }
