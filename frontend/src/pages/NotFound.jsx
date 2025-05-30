e3631a4c3204fc99e7baca57fc8dfb178ce3a4c4
Trent Wilkins <trenttwilkins02@gmail.com>
2025-05-30 11:54:35 -0400

    pcl cat

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
-
+import pickleImage from '../assets/pickle.png';
 
 export default function NotFound() {
 
 
     return (
-        <div>404 Page not found!</div>
+        <>
+            <div>404 Page not found!</div>
+            <img src={pickleImage} alt="dang, even the image isn't found!" />
+        </>
     )
 }
