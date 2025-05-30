dec0be11a5bf00664d6eccb62e77f3fa7de233b0
NathanF <nfant01@gmail.com>
2025-05-30 10:59:13 -0500

    nathan and dan, m, zipcode form + lat/long parsing

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -9,14 +9,31 @@ export default function CreateListing() {
     const [listingTitle, setListingTitle] = useState("");
     const [listingDesc, setListingDesc] = useState("");
     const [listingPay, setListingPay] = useState("");
+    const [zipcode, setZipcode] = useState("");
+    const [isValid, setIsValid] = useState(false); // This will evaluate the length/characters of zipcode
 
+    const getCoordsFromZipcode = async (zipcode) => {
+        const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${zipcode},+united+states&key=772594138f0f41488225603a3fd8ca9c`);
 
+        const data = await response.json();
+        if (data.results.length > 0) {
+            const { lat, long } = data.results[0].geometry;
+            return { lat, long };
+        } else {
+            throw new Error("No coordinates found for Zipcode");
+        }
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const { lat, long } = await getCoordsFromZipcode(zipcode)
+
         const listingJson = {
             title: listingTitle,
             description: listingDesc,
+            latitude: lat,
+            longitude: long,
             price: listingPay,
             poster_id: user?.user_id,
         }
@@ -42,33 +59,40 @@ export default function CreateListing() {
                 <h2>Create a job posting</h2>
                 <form className="listing-form" onSubmit={(e) => handleSubmit(e)} >
                     Job Title<input
-                    className="input-box"
-                    placeholder="Job..."
-                    value={listingTitle}
-                    onChange={(e) => {
-                        setListingTitle(e.target.value)
-                    }}
-                    required
-                    />
+                        className="input-box"
+                        placeholder="Job"
+                        value={listingTitle}
+                        onChange={(e) => {
+                            setListingTitle(e.target.value)
+                        }}
+                        required/>
                     Job Description<textarea
-                    className="input-box textarea-box"
-                    placeholder="Description..."
-                    value={listingDesc}
-                    onChange={(e) => {
-                        setListingDesc(e.target.value)
-                    }}
-                    rows={6}
-                    required
-                     />
+                        className="textarea-box input-box"
+                        placeholder="Description"
+                        value={listingDesc}
+                        onChange={(e) => {
+                            setListingDesc(e.target.value)
+                        }}
+                        rows={6}
+                        required/>
+                    Job Zipcode<input
+                        className="input-box"
+                        placeholder="5 Digit Zipcode"
+                        type="text"
+                        value={zipcode}
+                        onChange={(e) => {
+                            setZipcode(e.target.value)
+                        }}
+                        required={true} />
                     Job Pay<input
-                    className="input-box"
-                    placeholder="Pay..."
-                    value={listingPay}
-                    onChange={(e) => {
-                        setListingPay(e.target.value)
-                    }}
-                    required
-                    />
+                        className="input-box"
+                        placeholder="Pay"
+                        value={listingPay}
+                        onChange={(e) => {
+                            setListingPay(e.target.value)
+                        }}
+                        required={true}
+                        />
                     <button type="submit" className="create-button">Post job</button>
 
                 </form>
