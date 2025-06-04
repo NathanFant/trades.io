3c701bc2980a24abbbcb468f1363b3c7f3261ba8
ryan brown <rybrowntx@protonmail.com>
2025-06-04 10:33:10 -0600

    fixed syntax to make filter/serach working again

--- frontend/src/components/SearchFilterBar.jsx
+++ frontend/src/components/SearchFilterBar.jsx
@@ -2,7 +2,7 @@ import Searchbar from "./Searchbar";
 import FilterBySkill from "./FilterBySkill";
 
 
-export default function SearchFilterBar(setSearchTerm, searchTerm, setFilterTerm) {
+export default function SearchFilterBar({ setSearchTerm, searchTerm, setFilterTerm }) {
   return (
     <div className="search-filter-bar">
       <Searchbar setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
