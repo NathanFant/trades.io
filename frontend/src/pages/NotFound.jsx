9fcae71bd02e16c76cd29594c92c5ad8c55aeee8
Trent Wilkins <trenttwilkins02@gmail.com>
2025-06-05 12:49:19 -0400

    Pickle? Nah man ion want no d*** pickle

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,12 +1,22 @@
 import pickleImage from '../assets/pickle.png';
+import pickleWav from '../assets/sound/pickle.wav'
 
 export default function NotFound() {
+
+    function play() {
+        new Audio(pickleWav).play()
+    }
 
 
     return (
         <div className='four-o-four'>
-            <h1 >404 Page not found!</h1>
-            <img src={pickleImage} alt="dang, even the image isn't found!" />
+            <h1>404 Page not found!</h1>
+            <img
+            title='🥒 click me 🥒'
+            src={pickleImage}
+            alt="dang, even the image isn't found!"
+            onClick={play}
+            />
         </div>
     )
 }
